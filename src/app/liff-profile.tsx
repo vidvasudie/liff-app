8c229c6d56e72956658055f6486524f2e481bd42
vidvasudie <vidvasudie@gmail.com>
2025-07-16 10:34:28 +0700

    แก้ไข interface

--- src/app/liff-profile.tsx
+++ src/app/liff-profile.tsx
@@ -6,7 +6,7 @@ interface Profile {
 	userId: string;
 	displayName: string;
 	pictureUrl?: string;
-	statusMessage?: string;
+	//statusMessage?: string;
 }
 
 export default function LiffProfile() {
